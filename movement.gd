00b9b0fd462a74a6c6c35bc050697d9431d1e0d7
IvanKorchmit <criperadmiral92@gmail.com>
2023-06-04 18:52:59 +0300

    Change the script

--- movement.gd
+++ movement.gd
@@ -4,15 +4,25 @@ extends CharacterBody2D
 @export var speed : float = 30
 @export var visuals : Sprite2D
 
-@export var rotations : Array[Texture]
+@export var rotations : Array[Texture2D]
 
-func rotate_to_mouse():
+func get_degrees_to_mouse():
 	var mouse_pos : Vector2 = get_global_mouse_position()
 	var dir : Vector2 = (position - mouse_pos)
-	visuals.rotation_degrees = rad_to_deg(atan2(dir.y, dir.x))
+	set_sprite(rad_to_deg(atan2(dir.y, -dir.x)))
 
 func set_sprite(angle : float):
+	print("Before %d" % angle)
+	while angle < 0:
+		angle += 360
+		
+	var snap : float = 360 / len(rotations)
+	angle = round(angle / snap) * snap # snapping angle
 	
+	var index : int = round(angle / 360 * (len(rotations) - 1))
+	visuals.texture = rotations[index]
+	
+	print(angle)
 
 func handle_input(delta : float):
 	var direction : Vector2 = Vector2(0, 0)
@@ -24,7 +34,7 @@ func handle_input(delta : float):
 
 func _physics_process(delta):
 	handle_input(delta)
-	rotate_to_mouse()
+	get_degrees_to_mouse()
 	move_and_slide()
 
 
